8434b3c5bb10569cb8c64aa74cef011f785e0fc6
Dean Parker <dap2180@columbia.edu>
2018-06-04 18:24:49 -0400

    Added command-line functionality and changed sound output

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Stock2Sound
 Draws equity prices to convert into audio tracks.
+
+## To Use
+You need an API key from [Alpha Vantage](https://www.alphavantage.co/) (They are free) in a .txt file named "Key.txt" in the same directory of main.py
+```
+python main.py [StockTicker]
+```
+Will generate a .wav file with the name [StockTicker].wav in the same directory
+
+## Warning
+Watch your sound level, as the audio may be annoying to listen to, until I find a better way to construct the frequencies from the data.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import json
 import urllib3
 import numpy as np
+import sys
 from scipy.io.wavfile import write
 
 def normalize(nums):
@@ -16,6 +17,12 @@ def repeat(nums, n):
 	
 	return np.array(result)
 
+def construct(nums):
+	result = []
+	for i in range(0, len(nums)):
+		result += [nums[i] + nums[len(nums) - i - 1]]
+	return np.array(result)
+	
 def main():
 	apiKey = ""
 	with open("./Key.txt", "r") as keyFile:
@@ -24,16 +31,15 @@ def main():
 		
 
 	caller = urllib3.PoolManager()
-	url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=STRM&apikey=" + apiKey
+	url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + sys.argv[1] + "&apikey=" + apiKey
 
 	data = list(json.loads(caller.request("GET", url).data)['Time Series (Daily)'].values())
 	vals = []
 	for i in range(0, len(data)):
 		vals.insert(0, float(data[i]['4. close']) - float(data[i]['1. open']))
 
-	vals = normalize(np.array(vals))
-	print(repeat(vals, 1000))
-	write("./test.wav", 44100, repeat(vals, 1000))
+	vals = np.array(vals)
+	write("./" + sys.argv[1] + ".wav", 44100, normalize(construct(repeat(vals, 1000))))
 
 
 
